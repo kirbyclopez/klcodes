70d7b21172d8635fdf1d54b810558f0454ea8e23
kirbyclopez <kirbyclopez@gmail.com>
2022-06-11 00:20:32 +0800

    Initial website content while still under construction.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} alt='klcodes' />
+      <h1>Hey there! I'm Kirby!</h1>
+      <p>This portfolio is still under construction. Thank you for your patience.</p>
     </div>
   );
 }
