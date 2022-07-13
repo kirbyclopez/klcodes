8f4e7c17058cc5b3f5369121b896a8b1708eab1d
kirbyclopez <kirbyclopez@gmail.com>
2022-07-13 15:40:19 +0800

    Changed secondary color

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
-import logoSecondary from "./assets/images/klcodes-logo-secondary.png";
-import logoWhite from "./assets/images/klcodes-logo-white.png";
+import logo from "./assets/images/klcodes-logo-white.png";
 
 function App() {
   return (
     <div className="App">
-      <img src={logoWhite} alt="klcodes" className="banner-logo" />
+      <img src={logo} alt="klcodes" className="banner-logo" />
       <h1 className="header1">Hey there! I'm Kirby!</h1>
       <p className="subheading">
         This portfolio is still under construction. Thank you for your patience.
